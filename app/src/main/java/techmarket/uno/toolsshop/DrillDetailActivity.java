bb4827e5c23fe24e7ef57a4b511f02ddc134b94d
chigov2 <chigov@gmail.com>
2021-11-17 10:51:53 +0200

    final version

--- app/src/main/java/techmarket/uno/toolsshop/DrillDetailActivity.java
+++ app/src/main/java/techmarket/uno/toolsshop/DrillDetailActivity.java
@@ -3,9 +3,17 @@ package techmarket.uno.toolsshop;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrillDetailActivity extends AppCompatActivity {
+    private TextView tvTitle;
+    private TextView tvInfo;
+    private ImageView ivDrill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,22 @@ public class DrillDetailActivity extends AppCompatActivity {
         if (actionBar != null){
             actionBar.hide();
         }
+        tvInfo = findViewById(R.id.tvInfo);
+        tvTitle = findViewById(R.id.tvTitle);
+        ivDrill = findViewById(R.id.ivDrill);
+        Intent intent = getIntent();
+        //проверить - содержит данный интент нужную информацию
+        if (intent.hasExtra("title") && intent.hasExtra("info") && intent.hasExtra("resId")){
+            String title = intent.getStringExtra("title");
+            String info = intent.getStringExtra("info");
+            int resId = intent.getIntExtra("resId",-1);
+            tvTitle.setText(title);
+            tvInfo.setText(info);
+            ivDrill.setImageResource(resId);
+        }else{
+           Intent backToCategoryActivity = new Intent(this,DrillCategoryActivity.class);
+           startActivity(backToCategoryActivity);
+        }
+
     }
 }
